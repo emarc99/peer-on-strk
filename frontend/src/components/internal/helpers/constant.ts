704e6654582205172d3ae2317b2499b7b71ef76b
wheval <whevalmezeegbe@gmail.com>
2024-11-04 00:35:44 +0100

    fix: add leading zero to the contract address

--- frontend/src/components/internal/helpers/constant.ts
+++ frontend/src/components/internal/helpers/constant.ts
@@ -1,6 +1,6 @@
 export const ETH_SEPOLIA: string =
   "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
 export const STRK_SEPOLIA: string =
-  "0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d";
+  "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d";
 export const PROTOCOL_ADDRESS: string =
   "0x012908ace31594436016f42d3a618d95e1acc4bf89f32149eeec4cd900b49961";
