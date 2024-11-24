b55d27d9ff07e9fd7147dd9282825bce3430dbcb
Nze Shalom <106248195+Shalomnze@users.noreply.github.com>
2024-11-24 12:54:14 +0100

    Updated changes on the ranking table

--- frontend/src/app/app/Referral/page.tsx
+++ frontend/src/app/app/Referral/page.tsx
@@ -7,7 +7,7 @@ import Sidebar from "../sidebar";
 import RankingTable from "@/components/RankingTable";
 
 const ReferralBoards = () => {
-    
+
     return (
         <main className="bg-[#F5F5F5] min-h-screen text-black">
             <div className="flex">
@@ -17,12 +17,14 @@ const ReferralBoards = () => {
                     <Nav />
 
                     <div className="grid grid-cols-1 gap-4 px-10 ">
-                        <div className="flex flex-col mb-14">
+                        <div className="flex flex-col mb-10">
                             <h1 className=" text-[50px] font-semibold">Referral</h1>
                             <span className=" text-[25px] font-normal text-[#000000]">Invite friends and earn 10% of friend’s points</span>
                         </div>
-                        
-                        <RankingTable />
+
+                        <div className="mb-14">
+                            <RankingTable />
+                        </div>
                     </div>
                 </div>
             </div>
