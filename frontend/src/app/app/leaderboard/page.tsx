0bffa75cf527ba5bbe69875793de9be1fed2845a
austineblaise <austineblaise@gmail.com>
2024-11-24 22:46:20 +0100

    refactor(frontend): reuse existing components, improve leaderboard structure, and optimize mobile view

---
+++ frontend/src/app/app/leaderboard/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import Dashboard from "./dashboard";
+import Sidebar from "../sidebar";
+import Nav from "../Nav";
+
+export default function Home() {
+  return (
+    <main className="bg-[#F5F5F5]">
+      <div className="flex h-screen">
+        <Sidebar />
+
+        <div className="flex-1 flex flex-col h-full max-h-screen overflow-auto">
+          <Nav />
+          <main className="flex-1 p-4">
+            <Dashboard />
+          </main>
+        </div>
+      </div>
+    </main>
+  );
+}
